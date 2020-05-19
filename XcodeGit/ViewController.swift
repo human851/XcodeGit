633bc0dc1babe79153bd6176fb9e886d2371ce72
iosdev <iosdev@iosdevdemac.local>
2020-05-19 04:31:49 -0700

    Initial Commit

---
+++ XcodeGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeGit
+//
+//  Created by iosdev on 2020/5/19.
+//  Copyright © 2020年 Paul Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
